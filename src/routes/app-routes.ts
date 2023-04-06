386d5a263759c0ed4770bc32d99a4782ad0994df
Ricardo <rgranvilla@gmail.com>
2023-04-06 13:20:56 -0300

    feat: add app routes

--- src/routes/app-routes.ts
+++ src/routes/app-routes.ts
@@ -1,7 +1,15 @@
 import { FastifyInstance } from 'fastify';
 
+import { authRoutes } from '@modules/users/routes/auth-routes';
+import { passwordRoutes } from '@modules/users/routes/password-routes';
+import { usersRoutes } from '@modules/users/routes/users-routes';
+
 export async function appRoutes(app: FastifyInstance) {
   app.get('/', async (request, reply) => {
     reply.send({ message: 'Hello world' });
   });
+
+  app.register(usersRoutes);
+  app.register(authRoutes);
+  app.register(passwordRoutes);
 }
