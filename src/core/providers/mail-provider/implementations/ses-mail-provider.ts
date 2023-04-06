4b1b739b415e319dc18824ead5d82a3801978831
Ricardo <rgranvilla@gmail.com>
2023-04-06 13:05:42 -0300

    feat: add mail provider

---
+++ src/core/providers/mail-provider/implementations/ses-mail-provider.ts
@@ -0,0 +1,41 @@
+import { SES } from 'aws-sdk';
+import fs from 'fs';
+import handlebars from 'handlebars';
+import nodemailer, { Transporter } from 'nodemailer';
+
+import { env } from '@config/env';
+
+import { IMailProvider } from '../interface-mail-provider';
+
+export class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    this.client = nodemailer.createTransport({
+      SES: new SES({
+        apiVersion: '2010-12-01',
+        region: env.AWS_REGION,
+      }),
+    });
+  }
+
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string,
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString('utf-8');
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
+    await this.client.sendMail({
+      to,
+      from: 'Devtrails <rgranvilla@gmail.com>',
+      subject,
+      html: templateHTML,
+    });
+  }
+}
