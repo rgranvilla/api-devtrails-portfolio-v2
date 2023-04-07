9d1658922e88ec75f616d8300b113656cd72b2ea
Ricardo <rgranvilla@gmail.com>
2023-04-07 17:39:46 -0300

    feat: add update user skill route

--- src/application/routes/skills/skillsRoutes.ts
+++ src/application/routes/skills/skillsRoutes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify';
 
 import { createUserSkillController } from '@controllers/skills/create-user-skill/createUserSkillController';
+import { updateUserSkillController } from '@controllers/skills/update-user-skill/createUserSkillController';
 
 export async function skillsRoutes(app: FastifyInstance) {
   app.post('/:user_id/skills/create', createUserSkillController);
+  app.patch('/:user_id/skills/:skill_id/update', updateUserSkillController);
 }
