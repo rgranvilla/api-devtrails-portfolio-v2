d07421735b9c63e6100868587b5a3bd22cfc947e
Ricardo <rgranvilla@gmail.com>
2023-04-14 00:09:09 -0300

    feat: create course use case test

---
+++ src/domain/courses/use-cases/create-course/createCourseUseCase.spec.ts
@@ -0,0 +1,45 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import { CourseAlreadyExistsError } from '@errors/courses/courseAlreadyExistsError';
+
+import { InMemoryCourseRepository } from '@repositories/course/in-memory/inMemoryCourseRepository';
+
+import { CreateCourseUseCase } from './createCourseUseCase';
+
+let courseRepository: InMemoryCourseRepository;
+let sut: CreateCourseUseCase;
+
+describe('Create Course Use Case', () => {
+  beforeEach(() => {
+    courseRepository = new InMemoryCourseRepository();
+    sut = new CreateCourseUseCase(courseRepository);
+  });
+
+  it('should be able to create na course', async () => {
+    const { course } = await sut.execute({
+      name: 'Javascript',
+      date_start: new Date(),
+      user_id: 'an user id',
+    });
+
+    expect(course.id).toEqual(expect.any(String));
+  });
+
+  it('should not be able to create an course with name already exists', async () => {
+    const name = 'Javascript';
+
+    await sut.execute({
+      name,
+      date_start: new Date(),
+      user_id: 'an user id',
+    });
+
+    await expect(() =>
+      sut.execute({
+        name,
+        date_start: new Date(),
+        user_id: 'an user id',
+      }),
+    ).rejects.toBeInstanceOf(CourseAlreadyExistsError);
+  });
+});
